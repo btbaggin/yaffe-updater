8a2a00b8004e5791ccd0c5155ff8958123291865
Allex Jensen <allex.jen@outlook.com>
2022-03-08 16:32:08 -0700

    Create updating program

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+//Must match yaffe-rs.main.rs
+const UPDATE_FILE_PATH: &'static str = "./yaffe-rs.update";
+
+fn main() {
+    println!("Applying patch file");
+    if std::path::Path::new(UPDATE_FILE_PATH).exists() {
+        let args: Vec<String> = std::env::args().collect();
+
+        if let Err(e) = std::fs::rename(UPDATE_FILE_PATH, args[1].clone()) {
+            println!("{:?}", e);
+        }
+
+        if let Err(e) = std::process::Command::new(args[1].clone()).spawn() {
+            println!("{:?}", e);
+        }
+    }
+}
